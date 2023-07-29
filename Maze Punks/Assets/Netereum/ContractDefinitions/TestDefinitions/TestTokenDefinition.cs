0e664453f6d939fba63b6b54ababc11e178645cb
boraozenbirkan <b.ozenbirkan@gmail.com>
2023-07-30 02:53:06 +0300

    Nethereum Imported

---
+++ Maze Punks/Assets/Netereum/ContractDefinitions/TestDefinitions/TestTokenDefinition.cs
@@ -0,0 +1,308 @@
+using System;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Numerics;
+using Nethereum.Hex.HexTypes;
+using Nethereum.ABI.FunctionEncoding.Attributes;
+using Nethereum.Web3;
+using Nethereum.RPC.Eth.DTOs;
+using Nethereum.Contracts.CQS;
+using Nethereum.Contracts;
+using System.Threading;
+
+namespace ContractDefinitions.Contracts.testToken.ContractDefinition
+{
+    public partial class TestTokenDeployment : TestTokenDeploymentBase
+    {
+        public TestTokenDeployment() : base(BYTECODE) { }
+        public TestTokenDeployment(string byteCode) : base(byteCode) { }
+    }
+
+    public class TestTokenDeploymentBase : ContractDeploymentMessage
+    {
+        public static string BYTECODE = "0x";
+        public TestTokenDeploymentBase() : base(BYTECODE) { }
+        public TestTokenDeploymentBase(string byteCode) : base(byteCode) { }
+
+    }
+
+    public partial class AllowanceFunction : AllowanceFunctionBase { }
+
+    [Function("allowance", "uint256")]
+    public class AllowanceFunctionBase : FunctionMessage
+    {
+        [Parameter("address", "owner", 1)]
+        public virtual string Owner { get; set; }
+        [Parameter("address", "spender", 2)]
+        public virtual string Spender { get; set; }
+    }
+
+    public partial class ApproveFunction : ApproveFunctionBase { }
+
+    [Function("approve", "bool")]
+    public class ApproveFunctionBase : FunctionMessage
+    {
+        [Parameter("address", "spender", 1)]
+        public virtual string Spender { get; set; }
+        [Parameter("uint256", "amount", 2)]
+        public virtual BigInteger Amount { get; set; }
+    }
+
+    public partial class BalanceOfFunction : BalanceOfFunctionBase { }
+
+    [Function("balanceOf", "uint256")]
+    public class BalanceOfFunctionBase : FunctionMessage
+    {
+        [Parameter("address", "account", 1)]
+        public virtual string Account { get; set; }
+    }
+
+    public partial class BurnFunction : BurnFunctionBase { }
+
+    [Function("burn")]
+    public class BurnFunctionBase : FunctionMessage
+    {
+        [Parameter("uint256", "amount", 1)]
+        public virtual BigInteger Amount { get; set; }
+    }
+
+    public partial class BurnFromFunction : BurnFromFunctionBase { }
+
+    [Function("burnFrom")]
+    public class BurnFromFunctionBase : FunctionMessage
+    {
+        [Parameter("address", "account", 1)]
+        public virtual string Account { get; set; }
+        [Parameter("uint256", "amount", 2)]
+        public virtual BigInteger Amount { get; set; }
+    }
+
+    public partial class DecimalsFunction : DecimalsFunctionBase { }
+
+    [Function("decimals", "uint8")]
+    public class DecimalsFunctionBase : FunctionMessage
+    {
+
+    }
+
+    public partial class DecreaseAllowanceFunction : DecreaseAllowanceFunctionBase { }
+
+    [Function("decreaseAllowance", "bool")]
+    public class DecreaseAllowanceFunctionBase : FunctionMessage
+    {
+        [Parameter("address", "spender", 1)]
+        public virtual string Spender { get; set; }
+        [Parameter("uint256", "subtractedValue", 2)]
+        public virtual BigInteger SubtractedValue { get; set; }
+    }
+
+    public partial class IncreaseAllowanceFunction : IncreaseAllowanceFunctionBase { }
+
+    [Function("increaseAllowance", "bool")]
+    public class IncreaseAllowanceFunctionBase : FunctionMessage
+    {
+        [Parameter("address", "spender", 1)]
+        public virtual string Spender { get; set; }
+        [Parameter("uint256", "addedValue", 2)]
+        public virtual BigInteger AddedValue { get; set; }
+    }
+
+    public partial class MintFunction : MintFunctionBase { }
+
+    [Function("mint")]
+    public class MintFunctionBase : FunctionMessage
+    {
+        [Parameter("address", "to", 1)]
+        public virtual string To { get; set; }
+        [Parameter("uint256", "amount", 2)]
+        public virtual BigInteger Amount { get; set; }
+    }
+
+    public partial class NameFunction : NameFunctionBase { }
+
+    [Function("name", "string")]
+    public class NameFunctionBase : FunctionMessage
+    {
+
+    }
+
+    public partial class OwnerFunction : OwnerFunctionBase { }
+
+    [Function("owner", "address")]
+    public class OwnerFunctionBase : FunctionMessage
+    {
+
+    }
+
+    public partial class RenounceOwnershipFunction : RenounceOwnershipFunctionBase { }
+
+    [Function("renounceOwnership")]
+    public class RenounceOwnershipFunctionBase : FunctionMessage
+    {
+
+    }
+
+    public partial class SymbolFunction : SymbolFunctionBase { }
+
+    [Function("symbol", "string")]
+    public class SymbolFunctionBase : FunctionMessage
+    {
+
+    }
+
+    public partial class TotalSupplyFunction : TotalSupplyFunctionBase { }
+
+    [Function("totalSupply", "uint256")]
+    public class TotalSupplyFunctionBase : FunctionMessage
+    {
+
+    }
+
+    public partial class TransferFunction : TransferFunctionBase { }
+
+    [Function("transfer", "bool")]
+    public class TransferFunctionBase : FunctionMessage
+    {
+        [Parameter("address", "to", 1)]
+        public virtual string To { get; set; }
+        [Parameter("uint256", "amount", 2)]
+        public virtual BigInteger Amount { get; set; }
+    }
+
+    public partial class TransferFromFunction : TransferFromFunctionBase { }
+
+    [Function("transferFrom", "bool")]
+    public class TransferFromFunctionBase : FunctionMessage
+    {
+        [Parameter("address", "from", 1)]
+        public virtual string From { get; set; }
+        [Parameter("address", "to", 2)]
+        public virtual string To { get; set; }
+        [Parameter("uint256", "amount", 3)]
+        public virtual BigInteger Amount { get; set; }
+    }
+
+    public partial class TransferOwnershipFunction : TransferOwnershipFunctionBase { }
+
+    [Function("transferOwnership")]
+    public class TransferOwnershipFunctionBase : FunctionMessage
+    {
+        [Parameter("address", "newOwner", 1)]
+        public virtual string NewOwner { get; set; }
+    }
+
+    public partial class ApprovalEventDTO : ApprovalEventDTOBase { }
+
+    [Event("Approval")]
+    public class ApprovalEventDTOBase : IEventDTO
+    {
+        [Parameter("address", "owner", 1, true )]
+        public virtual string Owner { get; set; }
+        [Parameter("address", "spender", 2, true )]
+        public virtual string Spender { get; set; }
+        [Parameter("uint256", "value", 3, false )]
+        public virtual BigInteger Value { get; set; }
+    }
+
+    public partial class OwnershipTransferredEventDTO : OwnershipTransferredEventDTOBase { }
+
+    [Event("OwnershipTransferred")]
+    public class OwnershipTransferredEventDTOBase : IEventDTO
+    {
+        [Parameter("address", "previousOwner", 1, true )]
+        public virtual string PreviousOwner { get; set; }
+        [Parameter("address", "newOwner", 2, true )]
+        public virtual string NewOwner { get; set; }
+    }
+
+    public partial class TransferEventDTO : TransferEventDTOBase { }
+
+    [Event("Transfer")]
+    public class TransferEventDTOBase : IEventDTO
+    {
+        [Parameter("address", "from", 1, true )]
+        public virtual string From { get; set; }
+        [Parameter("address", "to", 2, true )]
+        public virtual string To { get; set; }
+        [Parameter("uint256", "value", 3, false )]
+        public virtual BigInteger Value { get; set; }
+    }
+
+    public partial class AllowanceOutputDTO : AllowanceOutputDTOBase { }
+
+    [FunctionOutput]
+    public class AllowanceOutputDTOBase : IFunctionOutputDTO 
+    {
+        [Parameter("uint256", "", 1)]
+        public virtual BigInteger ReturnValue1 { get; set; }
+    }
+
+
+
+    public partial class BalanceOfOutputDTO : BalanceOfOutputDTOBase { }
+
+    [FunctionOutput]
+    public class BalanceOfOutputDTOBase : IFunctionOutputDTO 
+    {
+        [Parameter("uint256", "", 1)]
+        public virtual BigInteger ReturnValue1 { get; set; }
+    }
+
+
+
+
+
+    public partial class DecimalsOutputDTO : DecimalsOutputDTOBase { }
+
+    [FunctionOutput]
+    public class DecimalsOutputDTOBase : IFunctionOutputDTO 
+    {
+        [Parameter("uint8", "", 1)]
+        public virtual byte ReturnValue1 { get; set; }
+    }
+
+
+
+
+
+
+
+    public partial class NameOutputDTO : NameOutputDTOBase { }
+
+    [FunctionOutput]
+    public class NameOutputDTOBase : IFunctionOutputDTO 
+    {
+        [Parameter("string", "", 1)]
+        public virtual string ReturnValue1 { get; set; }
+    }
+
+    public partial class OwnerOutputDTO : OwnerOutputDTOBase { }
+
+    [FunctionOutput]
+    public class OwnerOutputDTOBase : IFunctionOutputDTO 
+    {
+        [Parameter("address", "", 1)]
+        public virtual string ReturnValue1 { get; set; }
+    }
+
+
+
+    public partial class SymbolOutputDTO : SymbolOutputDTOBase { }
+
+    [FunctionOutput]
+    public class SymbolOutputDTOBase : IFunctionOutputDTO 
+    {
+        [Parameter("string", "", 1)]
+        public virtual string ReturnValue1 { get; set; }
+    }
+
+    public partial class TotalSupplyOutputDTO : TotalSupplyOutputDTOBase { }
+
+    [FunctionOutput]
+    public class TotalSupplyOutputDTOBase : IFunctionOutputDTO 
+    {
+        [Parameter("uint256", "", 1)]
+        public virtual BigInteger ReturnValue1 { get; set; }
+    }
+
+}
